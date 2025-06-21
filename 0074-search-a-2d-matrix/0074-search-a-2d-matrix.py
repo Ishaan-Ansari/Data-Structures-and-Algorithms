0339bf6f7731b8fa51216047f7b389f0703b2686
Ishaan Ansari <ishaanansari044@gmail.com>
2025-06-21 17:12:30 +0530

    Time: 0 ms (100%), Space: 18.4 MB (5.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,23 +1,39 @@
 class Solution:
-    def binSearch(self, row:List[int], target:int)->bool:
-        low, high = 0, len(row)-1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:  # O(Log(m*n))
+        m = len(matrix)
+        n = len(matrix[0])
+        i = 0
+        j = n-1
 
-        while low <= high:
-            mid = low+(high-low)//2
-            if row[mid] == target:
+        while(i<m and j>=0):
+            pos = matrix[i][j]
+            if pos == target:
                 return True
+            if pos>target:
+                j -= 1
+            elif pos<target:
+                i += 1
+        return False
 
-            if row[mid] < target:
-                low = mid+1
-            else:
-                high = mid-1
+    # def binSearch(self, row:List[int], target:int)->bool:       # O(R*Log(C))
+    #     low, high = 0, len(row)-1
+
+    #     while low <= high:
+    #         mid = low+(high-low)//2
+    #         if row[mid] == target:
+    #             return True
+
+    #         if row[mid] < target:
+    #             low = mid+1
+    #         else:
+    #             high = mid-1
         
-        return False
+    #     return False
 
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for idx in matrix:
-            row = idx
-            if self.binSearch(row, target):
-                return True
-        return False
+    # def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    #     for idx in matrix:
+    #         row = idx
+    #         if self.binSearch(row, target):
+    #             return True
+    #     return False
         
