cb45bee2854929a35ce6e863d77b533080e184f3
Ishaan Ansari <ishaanansari044@gmail.com>
2025-06-27 19:38:31 +0530

    Time: 7 ms (89.14%), Space: 18.9 MB (61.95%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.py
@@ -3,12 +3,13 @@ class Solution:
         idx = len(num)-1
         temp = ""
         
-        while(idx >= 0):
-            check_num = int(num[idx])
-            if check_num % 2 == 0:
-                idx -= 1
-            else:
-                break
+        while (idx >= 0) and (int(num[idx]) % 2 == 0):
+            idx -= 1
+            # check_num = int(num[idx])
+            # if check_num % 2 == 0:
+            #     idx -= 1
+            # else:
+            #     break
 
         # for i in range(0, idx+1):         # remove manual concat
         #     temp += num[i]
