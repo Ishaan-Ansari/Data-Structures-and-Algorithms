aa961e9d1665371a6e2e98a725a715013754c48a
Ishaan Ansari <mihansari.jh@gmail.com>
2025-07-26 14:35:48 +0530

    Time: 179 ms (56.64%), Space: 32.8 MB (69.39%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -6,21 +6,55 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None or head.next is None:
-            return head 
+            return head
+
+        left, right = self._split_list(head)
+
+        left_sorted = self.sortList(left)
+        right_sorted = self.sortList(right)
+
+        return self._merge_list(left_sorted, right_sorted)
+
+    def _split_list(self, head: Optional[ListNode]) -> tuple[ListNode, ListNode]:
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+        return head, mid
+
+    def _merge_list(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+
+        tail.next = l1 or l2
+        return dummy.next
+
+#     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if head is None or head.next is None:
+#             return head 
         
-        lst = []
-        curr = head
+#         lst = []
+#         curr = head
 
-        while curr:
-            lst.append(curr.val)
-            curr = curr.next
+#         while curr:
+#             lst.append(curr.val)
+#             curr = curr.next
 
-        # lst = sorted(lst)
-        lst.sort()
+#         # lst = sorted(lst)
+#         lst.sort()
 
-        curr = head
-        for num in lst:
-            curr.val = num
-            curr = curr.next
+#         curr = head
+#         for num in lst:
+#             curr.val = num
+#             curr = curr.next
         
-        return head
+#         return head
