7f9439df4a5e5711d07f56d9a5cebc2cd1dcef4c
Ishaan Ansari <mihansari.jh@gmail.com>
2025-08-09 13:44:37 +0530

    Time: 36 ms (78.73%), Space: 17.6 MB (63.59%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,9 @@
+from functools import lru_cache
+
 class Solution:
+    @lru_cache
     def fib(self, n: int) -> int:
-        if n==0 or n==1:
+        if n<2:
             return n
 
         return self.fib(n-1) + self.fib(n-2)
