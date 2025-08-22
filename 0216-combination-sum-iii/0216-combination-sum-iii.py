c414136288889ecbc4482d792b2ba7b34573ea44
Ishaan Ansari <mihansari.jh@gmail.com>
2025-08-22 14:45:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.45%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        ans = []
+        
+        def solve(curr, idx, pos, temp):
+            if curr == n and pos == k:
+                ans.append(temp[:])
+                return
+            
+            if curr > n or pos > k or idx >= len(nums):
+                return
+
+            temp.append(nums[idx])
+            solve(curr + nums[idx], idx+1, pos+1, temp)
+            temp.pop()
+            
+            solve(curr, idx+1, pos, temp) 
+
+        solve(0, 0, 0, [])
+        return ans
