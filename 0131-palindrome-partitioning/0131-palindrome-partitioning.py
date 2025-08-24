a56392462917ef94deec9f783dd5b2271124a5a9
Ishaan Ansari <mihansari.jh@gmail.com>
2025-08-24 14:17:11 +0530

    Time: 44 ms (77.59%), Space: 34.9 MB (8.19%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,19 +1,18 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans = []
-        idx = 0
 
-        def solve(idx, temp_lst):
+        def solve(idx, curr, temp):
             if idx == len(s):
-                ans.append(temp_lst[:])
-                return 
+                ans.append(temp[:])
+                return
 
-            for end in range(idx, len(s)):
-                sub = s[idx:end+1]
-                if sub == sub[::-1]:        
-                    temp_lst.append(sub)        
-                    solve(end + 1, temp_lst)    # recurse
-                    temp_lst.pop()              # backtrack
+            for i in range(idx, len(s)):
+                curr = s[idx:i+1]
+                if curr == curr[::-1]:
+                    temp.append(curr)
+                    solve(i+1, curr, temp)    # recurse
+                    temp.pop()  # backtrack
 
-        solve(0, [])
+        solve(0, "", [])
         return ans
