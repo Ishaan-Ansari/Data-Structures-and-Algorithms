8469c9179ae8dd8031101ad9733192727e2999b6
Ishaan Ansari <mihansari.jh@gmail.com>
2025-09-10 23:37:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (34.09%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        check = start ^ goal
-        check = bin(check)
-        check_without_prefix = check.replace("0b", "")
-
+        x = start ^ goal
         cnt = 0
-        for i in range(len(check_without_prefix)):
-            if check_without_prefix[i] == "1":
-                cnt += 1
+
+        while x:
+            x &= x-1
+            cnt += 1
 
         return cnt
+        
