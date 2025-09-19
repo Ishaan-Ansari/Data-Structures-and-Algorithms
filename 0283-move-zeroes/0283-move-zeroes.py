4be0df7446af592c22678b68efdd92a7eb416183
Ishaan Ansari <mihansari.jh@gmail.com>
2025-09-19 20:04:40 +0530

    Time: 3 ms (79.64%), Space: 18.9 MB (31.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,10 +7,10 @@ class Solution:
 
         for i in range(0, len(nums)):
             if nums[i] != 0:
-                nums[insertPos] = nums[i]
+                nums[insertPos], nums[i] = nums[i], nums[insertPos]
                 insertPos += 1
 
-        while insertPos < len(nums):
-            nums[insertPos] = 0
-            insertPos += 1
+        # while insertPos < len(nums):
+        #     nums[insertPos] = 0
+        #     insertPos += 1
         
