2d2f9bae908b76962dc2977543a8ab074e75f5f9
Ishaan Ansari <mihansari.jh@gmail.com>
2025-09-25 14:10:44 +0530

    Time: 1292 ms (8.54%), Space: 19.3 MB (87.64%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        ans = []
+        
+        for i in range(len(nums)):
+            flag = False
+            for j in range(i, len(nums)):
+                if nums[j] > nums[i]:
+                    ans.append(nums[j])
+                    flag = True
+                    break
+
+            if flag is not True:
+                for k in range(0, i):
+                    if nums[k] > nums[i]:
+                        ans.append(nums[k])
+                        break
+                else:
+                    ans.append(-1)
+
+        return ans
