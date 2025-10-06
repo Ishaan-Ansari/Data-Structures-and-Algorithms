1997525ed5ead7e8e5c3c32aedc754b255fb5aee
Ishaan Ansari <mihansari.jh@gmail.com>
2025-10-07 00:03:04 +0530

    Time: 0 ms (100%), Space: 18.2 MB (20.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,16 +1,13 @@
-from typing import List
-
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        mapp = {}
         stack = []
-        res = []
+        gre = {}
+
         for num in nums2:
-            while stack and num > stack[-1]:
-                mapp[stack.pop()] = num
+            while stack and stack[-1] < num:
+                gre[stack.pop()] = num
             stack.append(num)
-        for element in stack:
-            mapp[element] = -1
-        for i in nums1:
-            res.append(mapp[i])
-        return res
+        for num in stack:
+            gre[num] = -1
+
+        return [gre[num] for num in nums1]
