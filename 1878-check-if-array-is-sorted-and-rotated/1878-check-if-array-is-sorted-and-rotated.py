9f3cbaab0a793e8b7f1aa314808bfbec3d8dbaf1
Ishaan Ansari <mihansari.jh@gmail.com>
2025-10-13 13:49:01 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.9%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -3,9 +3,8 @@ class Solution:
         count_num_of_rotations = 0
         n = len(nums)
 
-        for i in range(0, n):
-            # make sure that the last element of the array is less than or equal to the first element.
-            if nums[i] > nums[(i+1) % n]:
+        for i in range(0, len(nums)):
+            if nums[i] > nums[(i+1)%n]:
                 count_num_of_rotations += 1
 
-        return count_num_of_rotations <= 1
+        return count_num_of_rotations<=1
