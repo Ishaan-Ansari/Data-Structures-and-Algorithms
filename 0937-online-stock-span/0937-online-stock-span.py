490d9d17cfdfa8b869125494b00305ca997c18eb
Ishaan Ansari <mihansari.jh@gmail.com>
2025-10-21 16:35:15 +0530

    Time: 68 ms (46.75%), Space: 22.7 MB (78.41%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -1,25 +1,19 @@
 class StockSpanner:
     def __init__(self):
         self.check = []
-
+        
     def next(self, price: int) -> int:
-        num_of_days = 1 # to count the current span
+        num_of_days = 1
 
-        while self.check and self.check[-1][0]<=price:
+        while self.check and self.check[-1][0] <= price:
             num_of_days += self.check.pop()[1]
-
+        
         self.check.append((price, num_of_days))
+
         return num_of_days
+
         
-        # if self.check:
-        #     for i in range(len(self.check)-1, -1, -1):
-        #         if price >= self.check[i]:
-        #             num_of_days += 1
-        #         else:
-        #             break
-        
-        # return num_of_days
-        
+
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
