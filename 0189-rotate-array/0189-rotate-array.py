5f0ade08de4472ff7d7b1541ef579a2a7d4e704f
Ishaan Ansari <mihansari.jh@gmail.com>
2025-10-26 09:46:49 +0530

    Time: 11 ms (20.69%), Space: 25.8 MB (21.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,15 +3,39 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        temp1 = nums[0:len(nums)-k%len(nums)]
-        temp2 = nums[len(nums)-k%len(nums):]
+        n = len(nums)
+        if n == 0:
+            return
 
-        nums.clear()
-        nums.extend(temp2)
-        nums.extend(temp1)
+        k %= n
+        if k == 0:
+            return
 
-              
+        def reverse(l:int, r:int):
+            while l<r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        # first reverse the whole array
+        reverse(0, n-1)
 
+        # then reverse the first k elems
+        reverse(0, k-1)
+
+        # reverse the remaining elems
+        reverse(k, n-1)
+
+        # -------------------------------------------------
+
+        # temp1 = nums[0:len(nums)-k%len(nums)]
+        # temp2 = nums[len(nums)-k%len(nums):]
+
+        # nums.clear()
+        # nums.extend(temp2)
+        # nums.extend(temp1)
+
+        #---------------------------------------------------
         
         # def rotate_once():
         #     last_elem = nums[-1]
