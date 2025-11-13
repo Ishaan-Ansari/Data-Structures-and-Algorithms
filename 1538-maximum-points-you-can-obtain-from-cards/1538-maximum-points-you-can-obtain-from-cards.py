4d9e75368f5610c8910a62ce4d22bd9dc82bb76f
Ishaan Ansari <mihansari.jh@gmail.com>
2025-11-14 01:19:31 +0530

    Time: 17 ms (94.77%), Space: 28.2 MB (20.43%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -1,20 +1,43 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         n = len(cardPoints)
-        left, right = 0, n-k
 
-        if n == k:
+        if n==k:
             return sum(cardPoints)
 
-        window_sum = sum(cardPoints[right:])
-        res = window_sum
+        left_sum = sum(cardPoints[:k])        
+        right_sum = 0
 
-        while right < n:
-            window_sum += cardPoints[left] - cardPoints[right]
-            res = max(res, window_sum)
-            left += 1
-            right += 1
+        left, right = k-1, n-1
+        maxi = left_sum
 
-        return res
+        while left >= 0:
+            window_sum = left_sum + right_sum
+            maxi = max(maxi, window_sum)
 
+            left_sum -= cardPoints[left]
+            right_sum += cardPoints[right]
+            left -= 1
+            right -= 1
+
+        maxi = max(maxi, right_sum)
+            
+        return maxi
+
+        # n = len(cardPoints)
+        # left, right = 0, n-k
+
+        # if n == k:
+        #     return sum(cardPoints)
+
+        # window_sum = sum(cardPoints[right:])
+        # res = window_sum
+
+        # while right < n:
+        #     window_sum += cardPoints[left] - cardPoints[right]
+        #     res = max(res, window_sum)
+        #     left += 1
+        #     right += 1
+
+        # return res
         
