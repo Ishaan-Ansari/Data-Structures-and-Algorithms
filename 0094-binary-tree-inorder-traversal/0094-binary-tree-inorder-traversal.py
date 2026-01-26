3c0740e60feca2ad809c5377f29f4d8236a81f0c
Ishaan Ansari <mihansari.jh@gmail.com>
2026-01-26 15:34:22 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.19%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -5,20 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.ans = []
-
-    def solve(self, root: Optional[TreeNode]) -> None:
-        if not root:
-            return
-        self.solve(root.left)
-        self.ans.append(root.val)
-        self.solve(root.right)
-
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
+            if not root:
+                return []
+
+            res = []
+            stack = []
+            node = root
+
+            while stack or node:
+                while node:
+                    stack.append(node)
+                    node = node.left
+                
+                node = stack.pop()
+                res.append(node.val)
+                node = node.right
+
+            return res
+
+
+    # def __init__(self):
+    #     self.ans = []
+
+    # def solve(self, root: Optional[TreeNode]) -> None:
+    #     if not root:
+    #         return
+    #     self.solve(root.left)
+    #     self.ans.append(root.val)
+    #     self.solve(root.right)
+
+    # def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    #     if not root:
+    #         return []
         
-        self.solve(root)
-        return self.ans
+    #     self.solve(root)
+    #     return self.ans
                 
