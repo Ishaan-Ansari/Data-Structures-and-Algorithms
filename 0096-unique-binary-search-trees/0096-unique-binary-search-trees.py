87e9142aae4a3fa8d8e368e8a4e828ba0d7d4d7d
Ishaan Ansari <mihansari.jh@gmail.com>
2026-03-04 22:43:58 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.36%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,18 @@
+class Solution:
+    def solve(self, n: int, dp: list[int]) -> int:
+        if n <= 1:
+            return 1
+
+        if dp[n] != -1:
+            return dp[n]
+
+        ans = 0
+        for i in range(1, n+1):
+            ans += self.solve(i-1, dp) * self.solve(n-i, dp)
+
+        dp[n] = ans
+        return dp[n]
+
+    def numTrees(self, n: int) -> int:
+        dp = [-1]*(n+1)
+        return self.solve(n, dp)
