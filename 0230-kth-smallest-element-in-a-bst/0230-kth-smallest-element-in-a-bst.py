463f6363d1c04fe35383ec70ec7806a99889ccd2
Ishaan Ansari <mihansari.jh@gmail.com>
2026-03-22 12:54:44 +0530

    Time: 0 ms (100%), Space: 22 MB (94.54%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,21 +7,51 @@
 
 class Solution:
     def __init__(self):
-        self.nums = []
+        self.count = 0
+        self.result = None
 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         if not root:
             return 0
 
-        self.inorder(root)
+        self.inorder(root, k)
 
-        return self.nums[k-1]
+        return self.result
 
-    def inorder(self, root):
-        if not root:
+    def inorder(self, root, k):
+        """Since we're traversing inorder we just need to keep a track of counter"""
+        # Base case: if node is null, or if we ALREADY found the result, stop.
+        if not root or self.result is not None:
             return
 
-        self.inorder(root.left)
-        self.nums.append(root.val)
-        self.inorder(root.right)
+        self.inorder(root.left, k)
+
+        self.count += 1
+        if self.count == k:
+            self.result = root.val
+            return
+
+        self.inorder(root.right, k)
+        
+
+# class Solution:
+#     def __init__(self):
+#         self.nums = []
+
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         if not root:
+#             return 0
+
+#         self.inorder(root)
+
+#         return self.nums[k-1]
+
+#     def inorder(self, root):
+#         """In BST inorder traversal gives sorted array"""
+#         if not root:
+#             return
+
+#         self.inorder(root.left)
+#         self.nums.append(root.val)
+#         self.inorder(root.right)
         
