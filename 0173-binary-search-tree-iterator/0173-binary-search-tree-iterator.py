c325df879f991b1c2a7f41fe13e001cc4e72a369
Ishaan Ansari <mihansari.jh@gmail.com>
2026-04-01 00:56:49 +0530

    Time: 8 ms (46.89%), Space: 25.7 MB (21.35%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -5,27 +5,46 @@
 #         self.left = left
 #         self.right = right
 class BSTIterator:
-
     def __init__(self, root: Optional[TreeNode]):
-        self.sorted_values = []
-        self.index = -1
-        
-        self._inorder_traversal(root)
+        self.arr = []
+        self._push_left(root)
 
-    def _inorder_traversal(self, root: Optional[TreeNode]):
-        if not root:
-            return 
-        
-        self._inorder_traversal(root.left)
-        self.sorted_values.append(root.val)
-        self._inorder_traversal(root.right)
+    def _push_left(self, root: Optional[TreeNode]):
+        while root is not None:
+            self.arr.append(root)
+            root = root.left
 
     def next(self) -> int:
-        self.index += 1
-        return self.sorted_values[self.index]
+        top_node = self.arr.pop()
 
+        if top_node.right:
+            self._push_left(top_node.right)
+        
+        return top_node.val
+        
     def hasNext(self) -> bool:
-        return (self.index+1) < len(self.sorted_values)
+        return len(self.arr) > 0
+
+    # def __init__(self, root: Optional[TreeNode]):
+    #     self.sorted_values = []
+    #     self.index = -1
+        
+    #     self._inorder_traversal(root)
+
+    # def _inorder_traversal(self, root: Optional[TreeNode]):
+    #     if not root:
+    #         return 
+        
+    #     self._inorder_traversal(root.left)
+    #     self.sorted_values.append(root.val)
+    #     self._inorder_traversal(root.right)
+
+    # def next(self) -> int:
+    #     self.index += 1
+    #     return self.sorted_values[self.index]
+
+    # def hasNext(self) -> bool:
+    #     return (self.index+1) < len(self.sorted_values)
 
 
 # Your BSTIterator object will be instantiated and called as such:
