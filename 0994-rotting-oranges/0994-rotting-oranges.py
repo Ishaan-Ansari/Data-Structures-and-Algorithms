0fd47df2906740d913ed08c054ca2c690ef94bef
Ishaan Ansari <mihansari.jh@gmail.com>
2026-05-09 17:54:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (41.06%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        time, fresh = 0, 0
-
         ROWS, COLS = len(grid), len(grid[0])
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
+        fresh = 0
+        time = 0
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j]==1:
                     fresh += 1
-                if grid[r][c] == 2:
-                    q.append([r, c])
 
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+                elif grid[i][j]==2:
+                    q.append([i, j])
+
+            
+        directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         while q and fresh > 0:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 r, c = q.popleft()
+                # check all four directions
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
-                    if (row < 0 or row == len(grid) or
-                        col < 0 or col == len(grid[0]) or
-                        grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-                    fresh -= 1
+                    row, col = r + dr, c + dc
+                    if (0 <= row < ROWS and 
+                        0 <= col < COLS and 
+                        grid[row][col] == 1):
+                        grid[row][col] = 2
+                        q.append([row, col])
+                        fresh -= 1
 
-            time += 1
-        
-        return time if fresh == 0 else -1
+            time += 1 
+
+        return time if fresh==0 else -1
