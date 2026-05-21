581718360718b76c8a4c35d586a3933a2b09f900
Ishaan Ansari <mihansari.jh@gmail.com>
2026-05-22 00:15:41 +0530

    Time: 77 ms (58.29%), Space: 21.9 MB (89.11%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+
+        q = deque()
+
+        for i in range(M):
+            for j in range(N):
+                if (grid[i][j]==1 and (i==0 or i==M-1 or j==0 or j==N-1)):
+                    q.append((i,j))
+                    grid[i][j]=0
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in directions:
+                new_r = r + dr
+                new_c = c + dc
+
+                if 0<=new_r<M and 0<=new_c<N and grid[new_r][new_c]==1:
+                    grid[new_r][new_c]=0
+                    q.append((new_r, new_c))
+
+        ans = 0
+        for i in range(M):
+            for j in range(N):
+                if (grid[i][j]==1):
+                    ans += 1
+
+        return ans
+
+
