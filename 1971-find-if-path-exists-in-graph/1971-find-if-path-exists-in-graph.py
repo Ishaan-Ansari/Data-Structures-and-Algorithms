3ba14b4569d59cc02f1b2571094887efcd74de05
Ishaan Ansari <mihansari.jh@gmail.com>
2026-07-08 14:13:31 +0530

    Time: 237 ms (89.56%), Space: 89 MB (81.89%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -2,28 +2,24 @@ from collections import defaultdict, deque
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # 1. Create a adjecency list
         adj_lst = defaultdict(list)
-
-        # create a adjacency list
         for u, v in edges:
             adj_lst[u].append(v)
             adj_lst[v].append(u)
 
-
+        visited = [0]*n
         q = deque([source])
-        visited = set([source])
 
         while q:
             node = q.popleft()
-
             if node == destination:
                 return True
 
-            for neighbor in adj_lst[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    q.append(neighbor)
-
-        
+            if not visited[node]:
+                visited[node] = 1
+                for neighbor in adj_lst[node]:
+                    if not visited[neighbor]:
+                        q.append(neighbor)
+                    
         return False
-
