60749b3e51a597c6c3586e39f70de6e0be4704ab
Ishaan Ansari <mihansari.jh@gmail.com>
2026-07-30 16:04:44 +0530

    Time: 3 ms (75.05%), Space: 20.3 MB (89.93%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,34 @@
+from collections import defaultdict
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prereq = {c:[] for c in range(numCourses)}
-        print(prereq)
+        # 1. create an adjecency list
+        adj_lst = defaultdict(list)
+        indegree = [0]*numCourses
 
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
+        for u, v in prerequisites:
+            adj_lst[v].append(u)
+            # If indegree[X] == 0, it means course X has absolutely zero prerequisites. You can take it on day one.
+            indegree[u] += 1
 
-        output = []
-        visit, cycle = set(), set()
+        node_with_no_incoming_edges = []
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                # we are actually storing course that have no prereq.
+                node_with_no_incoming_edges.append(i)
 
-        def dfs(crs):
-            if crs in cycle:
-                return False
+        course_order = []
+        while node_with_no_incoming_edges:
+            current_course = node_with_no_incoming_edges.pop()
+            course_order.append(current_course)
 
-            if crs in visit:
-                return True
+            for course in adj_lst[current_course]:
+                indegree[course] -= 1
 
-            cycle.add(crs)
-            for pre in prereq[crs]:
-                if dfs(pre) == False:
-                    return False
+                if indegree[course] == 0:
+                    node_with_no_incoming_edges.append(course)
 
-            cycle.remove(crs)
-            visit.add(crs)
-            output.append(crs)
-            return True
+        if len(course_order)==numCourses:
+            return course_order
 
-        for c in range(numCourses):
-            if dfs(c) == False:
-                return []
-
-        return output
+        return []         
