1d88d0debcfb8c8ba68218a5a10ee6dfaaeeb70c
Ishaan Ansari <mihansari.jh@gmail.com>
2026-09-11 02:01:11 +0530

    Add merge sort implementation in merge_sort.py
    Implement merge sort algorithm with a merge helper function.

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,1 +1,38 @@
+# merge sort
+def merge_sort(arr):
+  if len(arr) <= 1:
+    return arr
 
+  # PHASE 1: Divide 
+  mid = len(arr)//2
+  left_half = arr[:mid]
+  right_half = arr[mid:]
+
+  sorted_left = merge_sort(left_half)
+  sorted_right = merge_sort(right_half)
+
+  return merge(sorted_left, sorted_right)
+
+def merge(left, right):
+  sorted_results = []
+
+  i, j = 0, 0
+
+  while i<len(left) and j<len(right):
+    if left[i] < right[j]:
+      sorted_results.append(left[i])
+      i += 1
+    else:
+      sorted_results.append(right[j])
+      j += 1
+
+  # whatever is left
+  sorted_results.extend(left[i:])
+  sorted_results.extend(right[j:])
+
+  return sorted_results
+      
+
+
+arr = [10, 9, 4, 5, 1, 3, 2, 7, 6]
+print(merge_sort(arr))
